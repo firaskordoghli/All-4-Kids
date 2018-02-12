67cfa55e35f6f321a127044fe2848ad768136c72
rayen4r <36058081+rayen4r@users.noreply.github.com>
2018-02-12 20:47:59 +0100

    rayen classe test
    creation de classe test de rayen

---
+++ src/allforkids/gui/LoginController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package allforkids.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author casa-net
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField mail;
+    @FXML
+    private TextField password;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+   
+}
