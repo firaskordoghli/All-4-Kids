11216bd413e9ebebbbd681e381fadac0c2685a4c
slimhamdi95 <36002757+slimhamdi95@users.noreply.github.com>
2018-02-21 08:25:48 +0100

    rejounder

--- src/allforkids/gui/CovoiturageViewController.java
+++ src/allforkids/gui/CovoiturageViewController.java
@@ -6,8 +6,11 @@
 package allforkids.gui;
 
 import allforkids.entites.Evenement;
+import allforkids.entites.Session;
 import allforkids.entites.Transport;
+import allforkids.entites.Transportrejoindr;
 import allforkids.service.ServiceCovoiturage;
+import allforkids.service.ServiceTransportrejoindr;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -29,6 +32,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -65,6 +69,34 @@ public class CovoiturageViewController implements Initializable {
     private VBox detailsBox;
     @FXML
     private AnchorPane pane;
+    @FXML
+    private Label description;
+    @FXML
+    private Label departt;
+    @FXML
+    private Label arrivee;
+    @FXML
+    private Label tel;
+    @FXML
+    private Label frais;
+    @FXML
+    private Label typee;
+    @FXML
+    private Label placee;
+    @FXML
+    private Label tttype;
+    @FXML
+    private AnchorPane details;
+    @FXML
+    private TableView<Transport> tableTransport1;
+    @FXML
+    private TableColumn<Transport, String> depart1;
+    @FXML
+    private TableColumn<Transport, String> arrivé1;
+    @FXML
+    private TableColumn<Transport, String> place1;
+    @FXML
+    private Button load1;
     /**
      * Initializes the controller class.
      */
@@ -76,10 +108,13 @@ public class CovoiturageViewController implements Initializable {
     @FXML
     public void loadDataFromDatabase(ActionEvent event) {
        showTransport();
+       showTransportHistor();
+       
     }
     public void showTransport()
     {
      // TODO
+        details.setVisible(false);
         data = FXCollections.observableArrayList();
         data.addAll(s.selectCov());
         
@@ -94,14 +129,25 @@ public class CovoiturageViewController implements Initializable {
     @FXML
     public void showDetail(MouseEvent D)
     {
+        details.setVisible(true);
         // TODO
         row = tableTransport.getSelectionModel().getSelectedItem();
         region.setText(row.getRegion());
         ville.setText(row.getVille());
-        
+        departt.setText(row.getDepart());
+        arrivee.setText(row.getArrivé());
+        description.setText(row.getDescription());
+        tel.setText(row.getTelephone());
+        placee.setText(row.getPlace());
+        frais.setText(row.getFrais());
+        if(row.getType()==1){
+        typee.setText("Full");
+        }else{
+           typee.setVisible(false);
+           tttype.setVisible(false);
+        }
     }
     
-    @FXML
     public void ajoutCov(MouseEvent event) {
             try {
                 ajoutCovoiturage.getScene().getWindow().hide();
@@ -147,4 +193,30 @@ public class CovoiturageViewController implements Initializable {
                 window.show();
             
         }
+
+    @FXML
+    private void RejoindreCov(ActionEvent event) {
+       int u  = Session.getIdThisUser();
+        row = tableTransport.getSelectionModel().getSelectedItem();
+        int id = row.getId_transport();
+        Transportrejoindr tr = new Transportrejoindr(id,u);
+        ServiceTransportrejoindr str = new ServiceTransportrejoindr();
+        str.insrerTransportrejoindr(tr);
+        
+    }
+     public void showTransportHistor()
+    {
+           
+        details.setVisible(false);
+        data = FXCollections.observableArrayList();
+        ServiceTransportrejoindr str = new ServiceTransportrejoindr();
+        data.addAll(str.selectTransportrejoindhist(Session.getIdThisUser()));
+        
+        depart1.setCellValueFactory(new PropertyValueFactory<>("depart"));
+        arrivé1.setCellValueFactory(new PropertyValueFactory<>("arrivé"));
+        place1.setCellValueFactory(new PropertyValueFactory<>("place"));
+
+        tableTransport1.setItems(null);
+        tableTransport1.setItems(data);
+    }
 }
